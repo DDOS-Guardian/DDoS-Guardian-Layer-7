ee2c9529af15c57b8ee8876e01307c0a90c5558a
xlelord9292 <140418730+xlelord9292@users.noreply.github.com>
2024-06-17 10:13:00 -0400

    Update protection.lua

--- protection.lua
+++ protection.lua
@@ -1,10 +1,9 @@
 local whitelist = {
-    "127.0.0.1"
+    "127.0.0.1",
+    "109.71.253.231",
 }
 
-local blacklist = {
-    
-}
+local blacklist = {}
 
 local function ip_in_list(ip, list)
     for _, value in ipairs(list) do
@@ -18,20 +17,20 @@ local function ip_in_list(ip, list)
 end
 
 local function get_client_ip()
-       local cf_ip = ngx.var.http_cf_connecting_ip
+    local cf_ip = ngx.var.http_cf_connecting_ip
     if cf_ip then
         return cf_ip
     end
 
-     local real_ip = ngx.var.http_x_forwarded_for
+    local real_ip = ngx.var.http_x_forwarded_for
     if real_ip then
-              local first_ip = real_ip:match("([^,%s]+)")
+        local first_ip = real_ip:match("([^,%s]+)")
         if first_ip then
             return first_ip
         end
     end
 
-       return ngx.var.remote_addr
+    return ngx.var.remote_addr
 end
 
 local function generate_random_token()
@@ -53,62 +52,47 @@ local function delete_cookie()
     ngx.header['Set-Cookie'] = 'TOKEN=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly'
 end
 
-local logged_ips = {}
+local adaptive_rate_limits = {}  
+local blocked_ips = {}  
+local redirect_duration = 30  
 
--- Maximum allowed size of the log file in kilobytes (adjust as needed)
-local max_log_file_size_kb = 1024 
+local limit_dict = ngx.shared.ddos_guardian_limit_dict
 
-local function log_access(client_ip, request_uri, request_size, security_level, captcha_solved)
-    -- Check if this client IP has already been logged
-    if logged_ips[client_ip] then
-        return  -- If IP has been logged, skip logging again
+local function rate_limit_ip(ip)
+    if blocked_ips[ip] then
+        return true  
     end
 
-    local log_line = string.format('%s - %s - Requests: %s, Security: %s, Captcha: %s\n',
-        client_ip,
-        request_uri,
-        request_size,
-        security_level,
-        captcha_solved
-    )
-
-    local ddos_guardian_dir = "/var/log/ddos-guardian"
-    local ddos_guardian_log_file = ddos_guardian_dir .. "/access.log"
-
-    -- Ensure directory exists
-    local mkdir_command = "mkdir -p " .. ddos_guardian_dir
-    os.execute(mkdir_command)
+    local key = "rate:" .. ip
+    local current, err = limit_dict:get(key)
 
   
-    local ddos_guardian_file, ddos_guardian_err = io.open(ddos_guardian_log_file, "a")
-    if ddos_guardian_file then
-        -- Get current size of the log file in KB
-        local current_file_size_kb = ddos_guardian_file:seek("end") / 1024
-        
-
-        if current_file_size_kb >= max_log_file_size_kb then
-   
-            ddos_guardian_file:close()
-            ddos_guardian_file = io.open(ddos_guardian_log_file, "w")
-            if not ddos_guardian_file then
-                ngx.log(ngx.ERR, "Failed to truncate ddos-guardian access log file")
+    if adaptive_rate_limits[ip] then
+        if current and current >= adaptive_rate_limits[ip] then
+            blocked_ips[ip] = true 
+            ngx.log(ngx.ERR, "IP " .. ip .. " blocked due to suspected DDoS attack")
+            return true 
+        else
+            limit_dict:incr(key, 1)
+        end
+    else
+    
+        if current then
+            if current >= 1000 then
+                adaptive_rate_limits[ip] = current + 500  
+                return true  
+            else
+                limit_dict:incr(key, 1)
+            end
+        else
+            local success, err, forcible = limit_dict:set(key, 1, 60)
+            if not success then
+                ngx.log(ngx.ERR, "Failed to set rate limit for key: " .. key .. ", error: " .. err)
             end
         end
-
-  
-        local success, write_err = ddos_guardian_file:write(log_line)
-        if not success then
-            ngx.log(ngx.ERR, "Failed to write to ddos-guardian access log file: " .. write_err)
-        end
-        ddos_guardian_file:close()
-        
-    
-        logged_ips[client_ip] = true
-    else
-        ngx.log(ngx.ERR, "Failed to open ddos-guardian access log file: " .. ddos_guardian_err)
     end
+    return false
 end
-
 
 local function display_recaptcha(client_ip)
     ngx.log(ngx.ERR, "Displaying reCAPTCHA for IP: " .. client_ip)
@@ -122,7 +106,7 @@ local function display_recaptcha(client_ip)
             <script src="https://challenges.cloudflare.com/turnstile/v0/api.js?compat=recaptcha" async defer></script>
             <style>
                 html, body {
-                    height: 100%%;
+                    height: 100%;
                     margin: 0;
                     padding: 0;
                     background-color: #1b1c30;
@@ -135,8 +119,8 @@ local function display_recaptcha(client_ip)
                     border-radius: 3px;
                     text-align: center;
                     padding: 70px 0;
-                    width: 100%%;
-                    height: 100%%;
+                    width: 100%;
+                    height: 100%;
                 }
             </style>
             <script>
@@ -155,7 +139,6 @@ local function display_recaptcha(client_ip)
         </body>
         </html>
     ]])
-    log_access(client_ip, ngx.var.request_uri, ngx.var.request_length, "Medium", "false")
     ngx.exit(ngx.HTTP_FORBIDDEN)
 end
 
@@ -170,7 +153,7 @@ local function display_blacklist_page(client_ip)
             <title>Access Denied</title>
             <style>
                 html, body {
-                    height: 100%%;
+                    height: 100%;
                     margin: 0;
                     padding: 0;
                     background-color: #1b1c30;
@@ -183,8 +166,8 @@ local function display_blacklist_page(client_ip)
                     border-radius: 3px;
                     text-align: center;
                     padding: 70px 0;
-                    width: 100%%;
-                    height: 100%%;
+                    width: 100%;
+                    height: 100%;
                 }
             </style>
         </head>
@@ -197,69 +180,57 @@ local function display_blacklist_page(client_ip)
         </html>
     ]])
     delete_cookie()
-    log_access(client_ip, ngx.var.request_uri, ngx.var.request_length, "High", "false")
     ngx.exit(ngx.HTTP_FORBIDDEN)
 end
 
-local function sanitize_input(input)
-       return string.gsub(input, "[;%(')]", "")
+local function redirect_ip(ip)
+    ngx.log(ngx.ERR, "Redirecting IP " .. ip .. " to 109.71.253.231")
+    ngx.header["Location"] = "http://109.71.253.231"
+    ngx.status = ngx.HTTP_TEMPORARY_REDIRECT
+    ngx.exit(ngx.HTTP_TEMPORARY_REDIRECT)
 end
 
-
-local limit_dict = ngx.shared.ddos_guardian_limit_dict
-
-
+local function sanitize_input(input)
+    return string.gsub(input, "[;%(')]", "")
+end
 
 local function main()
     local client_ip = get_client_ip()
     local user_agent = ngx.var.http_user_agent or ""
+    local request_method = ngx.var.request_method
 
     ngx.log(ngx.ERR, "Client IP: " .. tostring(client_ip))
 
-    if ip_in_list(client_ip, blacklist) then
-        ngx.log(ngx.ERR, "Client IP is blacklisted: " .. client_ip)
-        display_blacklist_page(client_ip)
-        return
-    end
-
-
-
-    if ngx.var.request_uri:match("%.php$") or
-       ngx.var.request_uri:match("%.js$") or
-       ngx.var.request_uri:match("%.html$") or
-       ngx.var.request_uri:match("%.jsx$") or
-       ngx.var.request_uri:match("%.ts$") or
-       ngx.var.request_uri:match("%.tsx$") or
-       ngx.var.request_uri:match("%.png$") or
-       ngx.var.request_uri:match("%.jpg$") or
-       ngx.var.request_uri:match("%.jpeg$") or
-       ngx.var.request_uri:match("%.gif$") or
-       ngx.var.request_uri:match("%.svg$") or
-       ngx.var.request_uri:match("%.ico$") or
-       ngx.var.request_uri:match("%.css$") or
-       ngx.var.request_uri:match("%.woff$") or
-       ngx.var.request_uri:match("%.woff2$") or
-       ngx.var.request_uri:match("%.ttf$") or
-       ngx.var.request_uri:match("%.eot$") or
-       ngx.var.request_uri:match("%.otf$") or
-       ngx.var.request_uri:match("%.webp$") then
-        ngx.log(ngx.ERR, "Requested file type allowed")
-        log_access(client_ip, ngx.var.request_uri, ngx.var.request_length, "Low", "false")
-        return
-    end
-
     if ip_in_list(client_ip, whitelist) then
         ngx.log(ngx.ERR, "Client IP is whitelisted: " .. client_ip)
-        set_cookie() -- Generate token for whitelisted IP
-        log_access(client_ip, ngx.var.request_uri, ngx.var.request_length, "Low", "false")
+        set_cookie()  
         return
     end
 
+    if blocked_ips[client_ip] then
+        ngx.log(ngx.ERR, "IP " .. client_ip .. " blocked due to suspected DDoS attack")
+        ngx.status = ngx.HTTP_FORBIDDEN
+        ngx.say("Request blocked due to suspected DDoS attack")
+        ngx.exit(ngx.HTTP_FORBIDDEN)
+        return
+    end
+
+    if rate_limit_ip(client_ip) then
+        ngx.log(ngx.ERR, "Rate limit exceeded for IP: " .. client_ip)
+        display_recaptcha(client_ip)
+        return
+    end
+
+    if limit_dict:get("rate:" .. client_ip) >= 1000 then
+        redirect_ip(client_ip)
+        return
+    end
+
+ 
     if ngx.var.cookie_TOKEN then
         local token = ngx.var.cookie_TOKEN
         if #token >= 5 then
             ngx.log(ngx.ERR, "Valid token cookie found")
-            log_access(client_ip, ngx.var.request_uri, ngx.var.request_length, "Low", "true")
             return 
         else
             ngx.log(ngx.ERR, "Invalid token length, removing cookie")
@@ -267,6 +238,7 @@ local function main()
         end
     end
 
+   
     ngx.log(ngx.ERR, "Client IP is not whitelisted, showing reCAPTCHA")
     display_recaptcha(client_ip)
 end
